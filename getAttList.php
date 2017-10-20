13b39055c0de537de73ef197481d1633941d48d4
CJ <paul@takizo.com>
2017-10-20 15:11:22 +0800

    don't display cancel
    avoid display cancelled ticket. also loop through page for more than 50
    attendees

--- getAttList.php
+++ getAttList.php
@@ -1,27 +1,61 @@
 <?php
 
-$TOKEN = 'your-token-here';
-$EVENTID = 'your-event-id-here';
-$WHITELIST = array('Ricky Lee');
-$FILENAME = 'list.php';
+$TOKEN = 'Your-token';
+$EVENTID = 'Your-event-id';
+$WHITELIST = array('');
+$FILENAME = 'Your-list-file';
 
-$url = 'https://www.eventbriteapi.com/v3/events/'.$EVENTID.'/attendees/?token='.$TOKEN;
+$url = 'https://www.eventbriteapi.com/v3/events/'.$EVENTID.'/attendees/?status=attending&token='.$TOKEN;
 $result = file_get_contents($url, false, $context);
 $response = json_decode($result, true);
 
 $jj = 0;
 
+$totalpage = $response['pagination']['page_count'];
+
 
 foreach($response['attendees'] as $att)
 {
-	$fullname = $att['profile']['first_name'].' '.$att['profile']['last_name'];
+        $fullname = $att['profile']['name'];
 
-        if(!in_array($fullname, $WHITELIST))
+        if($att['cancelled'] != 1)
         {
-                $attList[$jj]['fullname'] = $fullname;
-                $attList[$jj]['company'] = $att['profile']['company'];
-                $attList[$jj]['jobTitle'] = $att['profile']['job_title'];
-                $jj++;
+                if(!in_array($fullname, $WHITELIST))
+                {
+
+                        $attList[$jj]['fullname'] = $fullname;
+                        $attList[$jj]['company'] = $att['profile']['company'];
+                        $attList[$jj]['jobTitle'] = $att['profile']['job_title'];
+                        $jj++;
+                }
+        }
+}
+
+if($totalpage > 1)
+{
+        for($d=1; $d<$totalpage; $d++)
+        {
+                $currentpage = $d+1;
+                $url = 'https://www.eventbriteapi.com/v3/events/'.$EVENTID.'/attendees/?token='.$TOKEN.'&page='.$currentpage;
+                $result = file_get_contents($url, false, $context);
+								$response = json_decode($result, true);
+
+                foreach($response['attendees'] as $att)
+                {
+                        $fullname = $att['profile']['name'];
+
+                        if($att['cancelled'] != 1)
+                        {
+                                if(!in_array($fullname, $WHITELIST))
+                                {
+
+                                        $attList[$jj]['fullname'] = $fullname;
+                                        $attList[$jj]['company'] = $att['profile']['company'];
+                                        $attList[$jj]['jobTitle'] = $att['profile']['job_title'];
+                                        $jj++;
+                                }
+                        }
+                }
         }
 }
 
